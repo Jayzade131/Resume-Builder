751830b1e05be8a77306546e50cdbe2159e23e99
jay zade <jaykumarzade@gmail.com>
2023-03-02 01:10:17 +0530

    update

---
+++ WebContent/js/script.js
@@ -0,0 +1,27 @@
+function addnewWex() {
+    let newNode=document.createElement('textarea');
+    newNode.classList.add("form-control");
+    newNode.classList.add("weField");
+    newNode.classList.add("mt-2");
+    newNode.setAttribute("rows",2);
+    newNode.setAttribute("placeholder","Enter here");
+
+    let weOb=document.getElementById("workexfield");
+    let weAddButtonOb=document.getElementById("weAddButton");
+    weOb.insertBefore(newNode,weAddButtonOb); 
+   
+}
+
+function addAcadQual() {
+    let newNode=document.createElement('textarea');
+    newNode.classList.add("form-control");
+    newNode.classList.add("aqField");
+    newNode.classList.add("mt-2");
+    newNode.setAttribute("rows",2);
+    newNode.setAttribute("placeholder","Enter here");
+
+    let aqOb=document.getElementById("acdquafield");
+    let weAddButtonOb2=document.getElementById("weAddButton2");
+    aqOb.insertBefore(newNode,weAddButtonOb2);
+    
+}
